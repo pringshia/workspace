c60a4edc8a7f1a03ef0d0913f91a7954a2f2df29
pringshia <pratik@ringshia.com>
2022-11-05 22:12:40 -0400

    Move over to submodule for shared neovim config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@
 # git clone --depth 1 https://github.com/wbthomason/packer.nvim \
 # ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+echo "Installing Neovim plugins..."
 nvim --headless --noplugin -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+echo "Installing Neovim plugins...done"
